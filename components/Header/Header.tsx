bc1f1b7470fff18bdda7837465856ce71906b042
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2024-12-13 19:23:20 +0500

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,43 +1,81 @@
-import React from 'react'
-import styles from '../Header/Header.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-
+import React from "react";
+import styles from "../Header/Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
   return (
     <div className={styles.containar}>
-
-        <div className={styles.main}>
-
-
-            <div className={styles.display}>
-
-                 <div className={styles.profile_div}>
-                    <Image className={styles.profile} src={'/images/done.png'} alt='profile-picture' width={100} height={100}/>
-                 </div>
-                 <div className={styles.tital_div}>
-                    <span className={styles.tital}>- Muhammad Faizan -</span>
-                 </div>
-                 <div className={styles.subject_div}>
-                    <h3><span className={styles.subject}>Front-End Web Devloper</span></h3>
-                 </div>
-                 <div className={styles.icons_div}>
-
-                    <Link href={'https://x.com/muhammadf4060'} target='_blank'><div className={styles.icon}><Image src={'/images/twitter-x.svg'} alt='twitter' width={25} height={25}/></div></Link>
-                    <Link href={'https://www.linkedin.com/in/muhammad-faizan-2541132b8'} target='_blank'><div className={styles.icon}><Image src={'/images/linkedin.svg'} alt='linkedin' width={25} height={25}/></div></Link>
-                    <Link href={'https://wa.me/+923406044359'} target='_blank'><div className={styles.icon}><Image src={'/images/whatsapp.svg'} alt='whatsapp' width={25} height={25}/></div></Link>
-                    <Link href={'https://www.instagram.com/fazii.963/'} target='_blank'><div className={styles.icon}><Image src={'/images/instagram.svg'} alt='instagram' width={25} height={25}/></div></Link>
-
-                 </div>
-
-            </div>
+      <div className={styles.main}>
 
 
+        <div className={styles.display}>
+          <div className={styles.profile_div}>
+            <Image
+              className={styles.profile}
+              src={"/images/done.png"}
+              alt="profile-picture"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={styles.tital_div}>
+            <span className={styles.tital}>- Muhammad Faizan -</span>
+          </div>
+          <div className={styles.subject_div}>
+            <h3>
+              <span className={styles.subject}>Front-End Web Devloper</span>
+            </h3>
+          </div>
+          <div className={styles.icons_div}>
+            <Link href={"https://x.com/muhammadf4060"} target="_blank">
+              <div className={styles.icon}>
+                <Image
+                  src={"/images/twitter-x.svg"}
+                  alt="twitter"
+                  width={25}
+                  height={25}
+                />
+              </div>
+            </Link>
+            <Link
+              href={"https://www.linkedin.com/in/muhammad-faizan-2541132b8"}
+              target="_blank"
+            >
+              <div className={styles.icon}>
+                <Image
+                  src={"/images/linkedin.svg"}
+                  alt="linkedin"
+                  width={25}
+                  height={25}
+                />
+              </div>
+            </Link>
+            <Link href={"https://wa.me/+923406044359"} target="_blank">
+              <div className={styles.icon}>
+                <Image
+                  src={"/images/whatsapp.svg"}
+                  alt="whatsapp"
+                  width={25}
+                  height={25}
+                />
+              </div>
+            </Link>
+            <Link href={"https://www.instagram.com/fazii.963/"} target="_blank">
+              <div className={styles.icon}>
+                <Image
+                  src={"/images/instagram.svg"}
+                  alt="instagram"
+                  width={25}
+                  height={25}
+                />
+              </div>
+            </Link>
+          </div>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- components/Work/Work.tsx
+++ components/Work/Work.tsx
@@ -1,35 +1,115 @@
-import React from 'react'
-import styles from '../Work/Work.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import styles from "../Work/Work.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 function Work() {
   return (
     <div className={styles.containar}>
+      
+      <div className={styles.tital_div}>
+        <h1 className={styles.tital}>Work</h1>
+      </div>
 
-                <div className={styles.tital_div}>
-                  <h1 className={styles.tital}>Work</h1>
-                </div>
+      <div className={styles.main}>
+        <div className={styles.grid_containar}>
 
-        <div className={styles.main}>
 
-            <div className={styles.grid_containar}>
-
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro1.png'} alt='one' height={500} width={500}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://faizan418-hackathon-project.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro2.png'} alt='two' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://mile2-assignment-css.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro3.png'} alt='three' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://mile3-project.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro4.png'} alt='four' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://static-interactive-resume-nine.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro5.png'} alt='five' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://number-gussing-game-mu.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/pro6.png'} alt='six' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'https://countdown-timer-app-beta.vercel.app/'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/portfolio-7.jpg'} alt='seven' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'#'} target='_blank'> Live Demo </Link></h1></div>
-                <div className={styles.card}><Image className={styles.images} src={'/images/portfolio-8.jpg'} alt='eight' height={300} width={300}/> <h1 className={styles.image_bg}><Link className={styles.last_li} href={'#'} target='_blank'> Live Demo </Link></h1></div>
-
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project1.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 01</h1>
+              <p className={styles.pera}>Resume Builder</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
             </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project2.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 02</h1>
+              <p className={styles.pera}>Basic E-commerce website</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project3.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 03</h1>
+              <p className={styles.pera}>Password Genrator</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project4.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 04</h1>
+              <p className={styles.pera}>PAK-Wheel E-commerce website</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project5.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 05</h1>
+              <p className={styles.pera}>Basic Ecommerce website</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project6.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 06</h1>
+              <p className={styles.pera}>Quiz app with login/signup</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project7.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 07</h1>
+              <p className={styles.pera}>Basic E-commerce website</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+          <div className={styles.card}>
+            <Image className={styles.img} src={'/images/project8.PNG'} alt="project-img" height={100} width={200}/>
+            <div className={styles.content}>
+              <h1 className={styles.heading}>project 08</h1>
+              <p className={styles.pera}>Todos app</p>
+                <Link href={''}>
+                    <button className={styles.live} type="button">Live demo</button>
+                </Link>
+            </div>
+          </div>
+
+
 
         </div>
-      
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
